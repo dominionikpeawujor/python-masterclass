cb840501fcc44a823c25ad0e652c731fc12291da
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-04-07 23:18:18 +0100

    Initial Commit
    This is the source code of the Python Masterclass Hosted on 7th of April
    by Unites Cisco Networking Academy.

---
+++ send_email.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+import smtplib
+
+SENDER_EMAIL = os.getenv('EMAIL')
+SENDER_PASSWORD = os.getenv('PASSWORD')
+
+SMTP = 'smtp.gmail.com'
+PORT = 587
+
+def send_email(receiver_email, subject, body):
+    message = f'Subject: {subject}\n\n{body}'
+    with smtplib.SMTP(SMTP, PORT) as server:
+        server.starttls()
+        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.sendmail(SENDER_EMAIL, receiver_email, message)
+
+#===========================================
+receiver_email = ['azeezmojeed001@gmail.com','matimojuadedayo@gmail.com', 'ossai.chris@yahoo.com', 'usmanadammikail@gmail.com', 'hartthex@gmail.com']
+subject = 'Python Masterclass - TEST'
+body = 'This is an email sent with Python.'
+
+send_email(receiver_email, subject, body)
